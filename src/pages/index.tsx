be2c64a5173c68e5e6e34dc6098da493992a1944
Matheus Oliveira <contato.mths@outlook.com>
2022-08-26 13:34:32 -0300

    style2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -156,6 +156,7 @@ export default function () {
           ))}
           {valorPorMes.map((valor, i) => (
             <Box key={i} fontWeight="bold">
+              {simbolosDasMoedas[moeda]}
               {mascaraDeMoeda(valor)}
             </Box>
           ))}
